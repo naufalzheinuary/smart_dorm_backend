c55df8371906f5f2c3b6f6dc1fc8fcc83562b6e7
naufalazriel <azrielnaufal5@gmail.com>
2026-05-12 14:17:09 +0700

    Use Firebase ENV variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,13 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../../config/firebase/serviceAccountKey.json");
+const serviceAccount = {
+
+    projectId: process.env.FIREBASE_PROJECT_ID,
+
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+};
 
 if (!admin.apps.length) {
 
