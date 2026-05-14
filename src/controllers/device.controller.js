4c184cfd95030dab106f5eaf671a1c04f115089a
naufalazriel <azrielnaufal5@gmail.com>
2026-05-14 21:07:13 +0700

    update controller

--- src/controllers/device.controller.js
+++ src/controllers/device.controller.js
@@ -1,14 +1,19 @@
+const admin = require("firebase-admin");
 const db = require("../config/firebase");
 
 const pingDevice = (req, res) => {
+
     res.json({
+
         success: true,
         message: "Device connected successfully"
     });
 };
 
 const unlockDoor = (req, res) => {
+
     res.json({
+
         success: true,
         message: "Door unlocked successfully"
     });
@@ -18,17 +23,60 @@ const receiveAccessLog = async (req, res) => {
 
     try {
 
+        const {
+
+            user_name
+
+        } = req.body;
+
+        // ================= FIND USER UID =================
+        const userSnapshot = await db
+            .collection("users")
+            .where("name", "==", user_name)
+            .limit(1)
+            .get();
+
+        let uid = null;
+        let role = "unknown";
+
+        if (!userSnapshot.empty) {
+
+            uid = userSnapshot.docs[0].id;
+
+            role =
+                userSnapshot.docs[0]
+                .data()
+                .role || "user";
+
+        } else {
+
+            console.log(
+                `USER NOT FOUND: ${user_name}`
+            );
+        }
+
+        // ================= ACCESS LOG DATA =================
         const data = {
 
+            uid,
+            role,
+
             ...req.body,
-            timestamp: new Date()
+
+            timestamp:
+                admin.firestore
+                .FieldValue
+                .serverTimestamp()
         };
 
-        await db.collection("access_logs").add(data);
+        await db
+            .collection("access_logs")
+            .add(data);
 
         console.log("ACCESS LOG SAVED");
 
         res.json({
+
             success: true,
             message: "Access log saved to Firebase",
             data
@@ -39,6 +87,7 @@ const receiveAccessLog = async (req, res) => {
         console.error(error);
 
         res.status(500).json({
+
             success: false,
             message: "Failed save access log"
         });
@@ -46,6 +95,7 @@ const receiveAccessLog = async (req, res) => {
 };
 
 module.exports = {
+
     pingDevice,
     unlockDoor,
     receiveAccessLog
